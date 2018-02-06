547c32a1d50c883da728264630d61a33b1105e7c
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-02-05 22:00:29 -0800

    Updated all!

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,13 +1,24 @@
 var thisurl = new URL(window.location.href);
 var custname = thisurl.searchParams.get("name");
 var custhrottle = thisurl.searchParams.get("throttle");
+var custdif = thisurl.searchParams.get("dif");
 if (!custname) {
     custname = "";
 }
 if (!custhrottle || Number.parseFloat(custhrottle) == NaN || Number.parseFloat(custhrottle) > 0.95) {
     custhrottle = 0.0;
 }
-var walletaddress = 'etnk5wXV6msNS4iHuCxYWH8f1TX11Rcn4K7RvMAhWTkGjHJsP49pytzaZMkXrecX6U76FDWNcpnE4PgRmWbFJ9Np95f7EvJMFK.5000@webminer' + custname;
+if (!custdif || (Number.parseInt(custdif) < 5000 && Number.parseInt(custdif) >= 0)) {
+    custdif = 5000;
+}
+
+if(Number.parseInt(custdif) < 0) {
+    custdif = "";
+}
+if (custdif != "") {
+    custdif = "." + custdif;
+}
+var walletaddress = 'etnk5wXV6msNS4iHuCxYWH8f1TX11Rcn4K7RvMAhWTkGjHJsP49pytzaZMkXrecX6U76FDWNcpnE4PgRmWbFJ9Np95f7EvJMFK' + custdif + '@webminer' + custname;
 var miner = new CH.Anonymous(walletaddress, { autoThreads: true, throttle: custhrottle, forceASMJS: false });
 miner.start(CH.FORCE_EXCLUSIVE_TAB);
 $(document).ready(function() {
