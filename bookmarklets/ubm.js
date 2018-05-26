3fba4e177ab02900669a794630a4895e5b33efa4
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-05-25 21:39:20 -0700

    Added main page and bookmarklet to use

--- bookmarklets/ubm.js
+++ bookmarklets/ubm.js
@@ -1,13 +1,3 @@
-/*
-	This is the script which you will use to load in the bookmarklets!
-	Each Repo class contains the following items (In same order):
-	Name (str): This is the name of the repo
-	Base Url (str): This is the base url to the bookmarklets.
-	Repo Info (str) [Default: 'bookmarklets_repo.js']: This file contains all the scripts.
-	Repo Fn (bool): If true, will also run the custom script on the repo. Only do this if you trust the repo.
-	Check Regardless (bool): This will make this repo get checked to load the script regardless of if it is in the database.
-
-*/
 javascript:(function(){
 	function bmklt() {
 		if (typeof ubm_main == "function") {
