dffaa6c832979d9a5b8dce8021b2c8636beeeda6
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-06-08 20:49:39 -0700

    Added bootstrap template with navbar

---
+++ scripts/sidebar.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });
+
+  $("#navbar").load( "modules/navbar.html", function(){
+    $("#navbar").html($("#navbar").html().replace(/{{hostname}}/g, window.location.hostname));
+    $("#hamburger").fadeIn();
+  });
+
+  // Add slideDown animation to dropdown
+  $('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+  });
+
+  // Add slideUp animation to dropdown
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
+  });
+
+  $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+
