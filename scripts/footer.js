e08ff3a59aa80cc0efee583db8c0f1256d13ce98
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-08-28 18:02:49 -0700

    Fixed footer issues.

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,8 @@
+if (typeof loc === "undefined") {
+  loc = ""
+}
 $(document).ready(function () {
-    $("#footer").load( "modules/footer.html", function(){
+    $("#footer").load( loc + "modules/footer.html", function(){
 	   $("#footer").fadeIn();
     });
 });
